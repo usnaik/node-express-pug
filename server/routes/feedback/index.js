b447a34a76aadc7fcdd50c82945e9841996fd8a0
usnaik <upendra_naik@yahoo.com>
2020-05-26 18:53:36 +0530

    Completed NodeJS Express and PUG templating project - Conference

---
+++ server/routes/feedback/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const router = express.Router();
+
+module.exports = (param) => {
+
+	const { feedbackService } = param;
+
+	router.get('/', async (req, res, next) => {
+		try {
+			const feedbacklist = await feedbackService.getList();
+			return res.render('feedback', {
+				page: 'Feedback',
+				feedbacklist,
+				success: req.query.success,
+			});
+		} catch (err) {
+			return next(err);
+		}
+	});
+
+	router.post('/', async (req, res, next) => {
+
+		try {
+			const fbName = req.body.fbName.trim();
+			const fbTitle = req.body.fbTitle.trim();
+			const fbMessage = req.body.fbMessage.trim();
+			const feedbacklist = await feedbackService.getList();
+
+			if (!fbName || !fbTitle || !fbMessage) {
+				return res.render('feedback', {
+					page: 'Feedback',
+					error: true,
+					fbTitle,
+					fbName,
+					fbMessage,
+					feedbacklist,
+				});
+			}
+			await feedbackService.addEntry(fbName, fbTitle, fbMessage);
+			return res.redirect('/feedback?success=true');
+		} catch (err) {
+			return next(err);
+		}
+	});
+
+	return router;
+};
---
+++ server/services/FeedbackService.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+class FeedbackService {
+
+	constructor(datafile) {
+		this.datafile = datafile;
+	}
+
+	async addEntry(name, title, message) {
+		try {
+			const data = await this.getData();
+			data.unshift({ name, title, message });
+			return writeFile(this.datafile, JSON.stringify(data));
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async getList() {
+		const data = await this.getData();
+		return data;
+	}
+
+	async getData(shortName) {
+		const data = await readFile(this.datafile, 'utf8');
+		if (!data) return [];
+		return JSON.parse(data);
+	}
+}
+
+module.exports = FeedbackService;
